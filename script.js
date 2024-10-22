b460c1ac8e913337d832ca8070c010471379b5ba
WiktorMarcin <w-marcinkowski@wp.pl>
2024-10-22 20:13:59 +0200

    Added operate function
    Sends further the operation

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(numberOne, numberTwo){
 function divide(numberOne, numberTwo){
     return numberOne / numberTwo;
 }
+
+
+function operate(numberOne, operator, numberTwo){
+    switch (operator) {
+        case "+":
+            return add(numberOne, numberTwo);
+        case "-":
+            return subtract(numberOne, numberTwo);
+        case "*":
+            return multiply(numberOne, numberTwo);
+        case "/":
+            return divide(numberOne, numberTwo);
+    }
+}
+
+console.log(operate(5,'-',4))
